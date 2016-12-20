731e666c221dc1a4ba7ae9c83f47f406ee1ab802
yanzhu <1037682870@qq.com>
2016-12-20 11:23:17 +0800

    b2

--- position.php
+++ position.php
@@ -189,25 +189,27 @@ $signPackage = $jssdk->GetSignPackage();
 	    ]
 	});
 	var latitude = 0,longitude = 0;
-	wx.getLocation({
-	    type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
-	    success: function (res) {
-	        latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
-	        longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
-	        var speed = res.speed; // 速度，以米/每秒计
-	        var accuracy = res.accuracy; // 位置精度
-	    }
-	});
-	wx.openLocation({
-	    latitude: latitude, // 纬度，浮点数，范围为90 ~ -90
-	    longitude: longitude, // 经度，浮点数，范围为180 ~ -180。
-	    name: '', // 位置名
-	    address: '', // 地址详情说明
-	    scale: 1, // 地图缩放级别,整形值,范围从1~28。默认为最大
-	    infoUrl: '' // 在查看位置界面底部显示的超链接,可点击跳转
-	});
-	$(".po-icon").html("定位中。。。");
-  	alert("纬度"+latitude+"经度"+longitude);
+	wx.ready(function(){
+		wx.getLocation({
+		    type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
+		    success: function (res) {
+		        latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+		        longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+		        var speed = res.speed; // 速度，以米/每秒计
+		        var accuracy = res.accuracy; // 位置精度
+		    }
+		});
+		wx.openLocation({
+		    latitude: latitude, // 纬度，浮点数，范围为90 ~ -90
+		    longitude: longitude, // 经度，浮点数，范围为180 ~ -180。
+		    name: '', // 位置名
+		    address: '', // 地址详情说明
+		    scale: 1, // 地图缩放级别,整形值,范围从1~28。默认为最大
+		    infoUrl: '' // 在查看位置界面底部显示的超链接,可点击跳转
+		});
+		$(".po-icon").html("定位中。。。");
+	  	alert("纬度"+latitude+"经度"+longitude);
+	  })	
   });
 </script>
 </body>
